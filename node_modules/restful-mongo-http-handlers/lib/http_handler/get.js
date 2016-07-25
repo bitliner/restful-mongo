94f4a9933ff93c67233d357d574663ab93b6cbb4
Andrea Mantovani <andreamnt94@gmail.com>
2016-07-25 10:55:08 +0200

    + Update version restful-mongo-http-handlers

--- node_modules/restful-mongo-http-handlers/lib/http_handler/get.js
+++ node_modules/restful-mongo-http-handlers/lib/http_handler/get.js
@@ -5,6 +5,59 @@ function HandlerHttpGet(dao) {
     this.dao = dao;
 }
 
+function convertIsoDateInDate(query) {
+    Logger.info('Runnin convertIsoDateInDate');
+    if (typeof value === 'object' && Array.isArray(query)) {
+	return query.map(function(el) {
+	    return convertIsoDateInDate(el);
+	});
+    }
+    if (!query) {
+	return null;
+    }
+    //Logger.info('--------------------------------->', query)
+    Object.keys(query).forEach(function(key) {
+	var value;
+	value = query[key];
+	if (typeof value === 'object') {
+	    query[key] = convertIsoDateInDate(query[key]);
+	} else if (typeof value === 'string' && value.indexOf('ISODate') >= 0) {
+	    value = value.replace(/ISODate\(./g, '');
+	    value = value.replace(/..$/g, '');
+	    query[key] = new Date(value);
+	}
+    });
+    return query;
+}
+
+function convertFakeObjectIdInObjectId(query) {
+    Logger.info('Runnin convertFakeObjectIdInObjectId');
+    if (typeof value === 'object' && Array.isArray(query)) {
+	return query.map(function(el) {
+	    return convertFakeObjectIdInObjectId(el);
+	});
+    }
+    if (!query) {
+	return null;
+    }
+    Object.keys(query).forEach(function(key) {
+	var value;
+	value = query[key];
+	//Logger.info('-->', value);
+	if (typeof value === 'object') {
+	    query[key] = convertFakeObjectIdInObjectId(query[key]);
+	} else if (typeof value === 'string' && value.indexOf('ObjectId') >= 0) {
+	    // Logger.info('----->', 'found objectid', value)
+	    value = value.replace(/ObjectId\(./g, '');
+	    value = value.replace(/..$/g, '');
+	    // Logger.info('-----> 2', 'found objectid', value)
+	    query[key] = new BSON.ObjectID(value);
+	    //Logger.info('Converted', query[key], query[key].constructor.name);
+	}
+    });
+    return query;
+}
+
 var _count = function(req, res) {
     var query = {},
     fields = {},
