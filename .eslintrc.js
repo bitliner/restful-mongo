f8d7cf6e9d4bd541a1ad403c62575eb657361939
bitliner <giovanni.gaglione@gmail.com>
2017-01-19 12:57:19 +0100

    +improved eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,17 @@
 module.exports = {
-    "extends": "google",
+	"extends": "google",
+	"rules": {
+		"max-len": ["error", {
+			"ignoreStrings": true
+		}],
+		"eol-last": ["error", "never"],
+		"require-jsdoc": ["error", {
+			"require": {
+				"FunctionDeclaration": false,
+				"MethodDefinition": false,
+				"ClassDeclaration": true,
+				"ArrowFunctionExpression": false
+			}
+		}]
+	},
 };
